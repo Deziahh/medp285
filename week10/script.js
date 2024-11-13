96e4f5bcfaa4abed76d12c2ced47337829cd17b5
Deziah Gayle <deziahgayle@gmail.com>
2024-11-13 15:12:55 -0500

    finished week10

---
+++ week10/script.js
@@ -0,0 +1,6 @@
+function displayWelcomeMessage() {
+    const welcomeDiv = document.getElementById("welcome-message");
+    welcomeDiv.style.display = "block"
+    welcomeDiv.innerHTML = "Explore projects, writings, and ideas blooming here!"
+}
+
